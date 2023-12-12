b5e3f8e406a9b34649ce2f6a426790c5a3bd3270
isk03300 <isk03300@naver.com>
2023-12-12 11:11:50 +0900

    add

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from resources.user import MovieUserResource
+
+
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(MovieUserResource  ,  '/user')
+
 
 if __name__ == '__main__' :
     app.run()
